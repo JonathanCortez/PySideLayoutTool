a556a19e32c0c3b8d55bcfb4fb4c468af82c6863
Jonathan Cortez <Heatalltheway@hotmail.com>
2023-03-13 01:24:39 -0400

    Import changes

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pysidelayouttool',
+      version='0.1.0',
+      description='A pyside2 parameter layout tool/application.',
+      url='https://github.com/JonathanCortez/PySideLayoutTool',
+      author='Jonathan Cortez',
+      author_email='jonathancdev@outlook.com',
+      license='LGPL-3.0',
+      packages=['Resources','UIEditorLib', 'UIEditorTemplates', 'UIEditorTemplates', 'UIEditorWindows']
+      )
