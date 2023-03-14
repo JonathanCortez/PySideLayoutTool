792fa5c053ccb03e03aa81d78762a82efe5d1890
Jonathan Cortez <Heatalltheway@hotmail.com>
2023-03-14 18:54:47 -0400

    Import changes

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name='PySide Layout Tool',
       author='Jonathan Cortez',
       author_email='jonathancdev@outlook.com',
       license='LGPL-3.0',
-      install_requires=['PySide2'],
+      install_requires=['PySide2', 'numpy'],
       packages=['Resources','UIEditorLib', 'UIEditorTemplates', 'UIEditorTemplates', 'UIEditorWindows']
       )
