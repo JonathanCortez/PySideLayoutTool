1a3c5b2c20c375537d4bca832ec295ac6a73797b
Jonathan Cortez <Heatalltheway@hotmail.com>
2023-03-21 23:09:15 -0400

    New updates to project ready for first pip version.

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 from setuptools import setup, find_packages
 
-setup(name='pysidelayouttool',
-      version='0.2.0',
-      description='A pyside2 parameter layout tool/application.',
+description = """PySide Layout Tool is an open-source graphical user interface (GUI) tool developed using Python and 
+the PySide2 library for creating and editing layouts in PySide-based applications. The tool provides a simple and 
+intuitive interface for designing and manipulating widgets and layouts, which are the building blocks of PySide 
+applications. The PySide Layout Tool allows users to create, edit, and save layouts in various file formats, 
+including JSON and XML. It also provides a preview mode that allows users to see how the layout will look. The tool 
+provides a wide range of layout options, including grid layout, horizontal layout, vertical layout, and stacked 
+layout. The tool is designed to simplify the layout creation process and make it more efficient. Users can drag and 
+drop widgets onto the canvas and adjust their position and size using the mouse. The tool also provides alignment and 
+spacing options to help users create a neat and organized layout. """
+
+
+setup(name='PySideLayoutTool',
+      version='0.2.1',
+      description='PySide Layout Tool is an open-source graphical user interface (GUI) tool developed using PySide2',
+      long_description=description,
+      maintainer='Jonathan Cortez',
       url='https://github.com/JonathanCortez/PySideLayoutTool',
       author='Jonathan Cortez',
       author_email='jonathancdev@outlook.com',
